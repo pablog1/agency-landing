bcf40076f7c95b62d680c0ead39adeb6df921789
PabloG <p.goldstoff@gmail.com>
2022-09-18 11:34:52 -0300

    sticky header

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,25 @@
-import { useState, ReactNode } from 'react';
+import { useState, ReactNode, useEffect } from 'react';
 
 type INavbarProps = {
   children: ReactNode;
 };
 const Sidebar = (props: INavbarProps) => {
   const [showSidebar, setShowSidebar] = useState(false);
-
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
   return (
     <>
       {showSidebar ? (
         <button
-          className="flex text-5xl  text-primary-400 items-center cursor-pointer fixed left-[23px] top-[32px] z-50 "
+          className={`${
+            offset > 30 ? `header-small` : `header-big`
+          } transition duration-300  flex text-5xl  text-primary-400 items-center cursor-pointer fixed left-[23px] z-50 `}
           onClick={() => setShowSidebar(!showSidebar)}
         >
           x
@@ -18,7 +27,9 @@ const Sidebar = (props: INavbarProps) => {
       ) : (
         <svg
           onClick={() => setShowSidebar(!showSidebar)}
-          className="fill-primary-400 md:hidden fixed z-30 flex items-center cursor-pointer left-4 top-14"
+          className={`${
+            offset > 30 ? `header-small` : `header-big`
+          } fill-primary-400 md:hidden fixed z-30 flex items-center cursor-pointer left-4 top-14`}
           viewBox="0 0 100 80"
           width="40"
           height="40"
@@ -37,14 +48,22 @@ const Sidebar = (props: INavbarProps) => {
         <ul className="mt-20 text-2xl font-semibold text-black">
           {props.children}
         </ul>
-        <style jsx>
-          {`
-            ul {
-              @apply list-none;
-            }
-          `}
-        </style>
       </div>
+      <style jsx>
+        {`
+          .header-big {
+            top: 2.8rem;
+            transition: 150ms ease;
+          }
+          .header-small {
+            top: 0.5rem;
+            transition: 150ms ease;
+          }
+          ul {
+            @apply list-none;
+          }
+        `}
+      </style>
     </>
   );
 };
--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -7,76 +9,91 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { AppConfig } from '../utils/AppConfig';
 import { Logo } from './Logo';
 
-const Header = () => (
-  <>
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    <Sidebar>
-      <li>
-        <Link href="/newpage">
-          <a>New page</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-          <a>GitHub</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/">
-          <a>Sign in</a>
-        </Link>
-      </li>
-    </Sidebar>
-    <section className="fixed top-0 z-10 w-full bg-white pb-6 background">
-      <div className="">
-        <Image
-          src="/assets/images/pixelated_background.png"
-          objectFit="cover"
-          layout="fill"
-          quality={65}
-          alt=""
-          className="background"
-        />
-      </div>
-      <div className="bg-black text-white relative z-10">
+const Header = () => {
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <>
+      <Meta title={AppConfig.title} description={AppConfig.description} />
+      <Sidebar>
+        <li>
+          <Link href="/newpage">
+            <a>New page</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+            <a>GitHub</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Sign in</a>
+          </Link>
+        </li>
+      </Sidebar>
+      <section className="bg-black text-white relative z-10">
         <div className="c-container text-center">TopBar</div>
-      </div>
-      <div className="c-container pt-6">
-        <NavbarTwoColumns
-          logo={<Logo xl textColor="text-black" iconColor="text-black" />}
-        >
-          <li className="z-10">
-            <Link href="/newpage">
-              <a>New page</a>
-            </Link>
-          </li>
-          <li className="z-10">
-            <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-              <a>GitHub</a>
-            </Link>
-          </li>
-          <li className="z-10">
-            <Link href="/">
-              <a>Sign in</a>
-            </Link>
-          </li>
-        </NavbarTwoColumns>
-      </div>
-    </section>
-    <section className="mt-[122px]"></section>
-    <style jsx>
-      {`
-        background {
-          flex: 0 0 290px;
-          height: 190px;
-          position: relative;
-          background-size: cover;
-          background-repeat: repeat;
-          background-position: center center;
-        }
-      `}
-    </style>
-  </>
-);
+      </section>
+      <section
+        className={`${
+          offset > 30 ? `header-padding-small` : `header-padding-big`
+        } transition duration-300  sticky top-0 w-full bg-white background z-10`}
+      >
+        <style jsx>
+          {`
+            .header-padding-big {
+              padding: 1rem 0;
+              transition: 300ms ease;
+            }
+            .header-padding-small {
+              padding: 0.3rem 0;
+              transition: 300ms ease;
+            }
+          `}
+        </style>
+        <div className="">
+          <Image
+            src="/assets/images/pixelated_background.png"
+            objectFit="cover"
+            layout="fill"
+            quality={65}
+            alt=""
+            className="background"
+          />
+        </div>
+
+        <div className="c-container">
+          <NavbarTwoColumns
+            logo={<Logo xl textColor="text-black" iconColor="text-black" />}
+          >
+            <li className="z-10">
+              <Link href="/newpage">
+                <a>New page</a>
+              </Link>
+            </li>
+            <li className="z-10">
+              <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+                <a>GitHub</a>
+              </Link>
+            </li>
+            <li className="z-10">
+              <Link href="/">
+                <a>Sign in</a>
+              </Link>
+            </li>
+          </NavbarTwoColumns>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export { Header };
