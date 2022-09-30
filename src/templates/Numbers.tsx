53f5ad0c5a7949ca1b44bc1f9268ac2428c62c0d
PabloG <p.goldstoff@gmail.com>
2022-09-29 22:25:59 -0300

    fix

--- src/templates/Numbers.tsx
+++ src/templates/Numbers.tsx
@@ -1,16 +1,12 @@
 import { Number } from '../components/number';
 
 const Numbers = ({ content }: any) => {
+  console.log(content);
   return (
     <section className="relative c-container mt-16">
       <div className="flex flex-col md:flex-row items-center justify-center relative text-black">
-        {Object.keys(content.number).map((keyName, i) => (
-          <Number
-            key={i}
-            title={content.numbers[keyName].number}
-            src={`/${content.image}`}
-            alt="xxxx"
-          />
+        {Object.keys(content.benefits).map((keyName, i) => (
+          <Number key={i} alt="xxxx" />
         ))}
       </div>
     </section>
