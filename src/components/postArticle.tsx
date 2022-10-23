541b899393d764823085a9e166fda9ddb7981d26
PabloG <p.goldstoff@gmail.com>
2022-10-23 10:43:12 -0300

    post articles

---
+++ src/components/postArticle.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+
+type PostArticleProps = {
+  article: any;
+};
+
+const MarkdownComponents: object = {
+  // Code will go here
+
+  p: (paragraph: { children?: boolean; node?: any }) => {
+    const { node } = paragraph;
+
+    if (node.children[0].tagName === 'img') {
+      const image = node.children[0];
+      const metastring = image.properties.alt;
+      const alt = metastring?.replace(/ *\{[^)]*\} */g, '');
+      const hasCaption = metastring?.toLowerCase().includes('{caption:');
+      const caption = metastring?.match(/{caption: (.*?)}/)?.pop();
+
+      return (
+        <div className="postImgWrapper flex justify-center my-8 h-[77vw] max-h-[450px] relative">
+          <Image
+            layout="fill"
+            src={`/${image.properties.src}`}
+            className="postImg object-contain w-full"
+            alt={alt}
+          />
+          {hasCaption ? (
+            <div className="caption" aria-label={caption}>
+              {caption}
+            </div>
+          ) : null}
+        </div>
+      );
+    }
+    return <p>{paragraph.children}</p>;
+  },
+};
+
+const PostArticle = (props: PostArticleProps) => (
+  <article className="c-container !max-w-screen-2xl mb-8">
+    <h1 className="h1 mt-32 mb-4 text-center !text-black">
+      {props.article.title}
+    </h1>
+    <picture className="flex justify-center py-12">
+      <Image
+        className="object-center object-contain pointer-events-none rounded-md"
+        src={`/${props.article.image}`}
+        alt={props.article.title}
+        objectFit="cover"
+        width="1200"
+        height="800"
+      />
+    </picture>
+
+    <ReactMarkdown
+      // eslint-disable-next-line react/no-children-prop
+      children={props.article.body}
+      components={MarkdownComponents}
+      className="!max-w-screen-lg mx-auto prose-p:mb-8"
+    />
+  </article>
+);
+
+export { PostArticle };
--- src/pages/case-study/[caseStudy].tsx
+++ src/pages/case-study/[caseStudy].tsx
@@ -1,43 +1,8 @@
 import fs from 'fs';
 
-import Image from 'next/image';
-import ReactMarkdown from 'react-markdown';
-
+import { PostArticle } from '../../components/postArticle';
 import { Footer } from '../../templates/Footer';
 import { Header } from '../../templates/Header';
-
-const MarkdownComponents: object = {
-  // Code will go here
-
-  p: (paragraph: { children?: boolean; node?: any }) => {
-    const { node } = paragraph;
-
-    if (node.children[0].tagName === 'img') {
-      const image = node.children[0];
-      const metastring = image.properties.alt;
-      const alt = metastring?.replace(/ *\{[^)]*\} */g, '');
-      const hasCaption = metastring?.toLowerCase().includes('{caption:');
-      const caption = metastring?.match(/{caption: (.*?)}/)?.pop();
-
-      return (
-        <div className="postImgWrapper flex justify-center my-8 h-[77vw] max-h-[450px] relative">
-          <Image
-            layout="fill"
-            src={`/${image.properties.src}`}
-            className="postImg object-contain w-full"
-            alt={alt}
-          />
-          {hasCaption ? (
-            <div className="caption" aria-label={caption}>
-              {caption}
-            </div>
-          ) : null}
-        </div>
-      );
-    }
-    return <p>{paragraph.children}</p>;
-  },
-};
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync('content/case_studies');
@@ -90,14 +55,7 @@ const Post = (props: {
         siteName={props.siteName}
         topBar={props.topBar}
       />
-      <article className="c-container !max-w-screen-2xl mb-8">
-        <h1 className="h1 mt-32 mb-4">{props.article.title}</h1>
-        <ReactMarkdown
-          // eslint-disable-next-line react/no-children-prop
-          children={props.article.body}
-          components={MarkdownComponents}
-        />
-      </article>
+      <PostArticle article={props.article} />
 
       <Footer logo={props.logo} siteName={props.siteName} />
     </>
