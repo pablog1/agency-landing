136feaac2f037393a26cfa288107da3550ce171c
PabloG <p.goldstoff@gmail.com>
2022-10-23 23:31:01 -0300

    Newsletter

---
+++ src/components/NewsletterSignup.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@material-tailwind/react';
+import Image from 'next/image';
+
+const NewsletterSignup = () => (
+  <section className="flex flex-col lg:flex-row justify-center items-center p-4 bg-black">
+    <div className="pb-2">
+      <Image
+        className="object-center invert"
+        src="/img/newsletter.svg"
+        alt="Newsletter"
+        height={40}
+        width={40}
+      />
+    </div>
+
+    <p className="font-bold text-center !text-white mb-4 lg:mb-0 lg:px-6 ">
+      Subscribe to our Newsletter
+    </p>
+    <div className="flex flex-row justify-center ">
+      <form>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          className="h-full rounded-md mr-2 outline-none placeholder:text-gray-600 pl-4 min-w-[300px]"
+        ></input>
+      </form>
+      <Button className="bg-primary-500  text-base">Join</Button>
+    </div>
+  </section>
+);
+
+export { NewsletterSignup };
--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import { Background } from '../background/Background';
+import { NewsletterSignup } from '../components/NewsletterSignup';
 import { CenteredFooter } from '../footer/CenteredFooter';
 import { Logo } from './Logo';
 
@@ -10,57 +11,61 @@ type FooterProps = {
 };
 
 const Footer = (props: FooterProps) => (
-  <Background color="bg-primary-400 text-white py-4">
-    <CenteredFooter
-      logo={
-        <div className="brightness-0 invert">
-          <Logo
-            textColor="text-white"
-            iconColor="text-white"
-            logo={props.logo}
-            siteName={props.siteName}
-          />
-        </div>
-      }
-      iconList={
-        <>
-          <Link href="https://www.linkedin.com/company/dangodigital">
-            <a aria-label="Linkedin">
-              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-              </svg>
-            </a>
+  <>
+    <NewsletterSignup />
+
+    <Background color="bg-primary-400 text-white py-4">
+      <CenteredFooter
+        logo={
+          <div className="brightness-0 invert">
+            <Logo
+              textColor="text-white"
+              iconColor="text-white"
+              logo={props.logo}
+              siteName={props.siteName}
+            />
+          </div>
+        }
+        iconList={
+          <>
+            <Link href="https://www.linkedin.com/company/dangodigital">
+              <a aria-label="Linkedin">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+              </a>
+            </Link>
+          </>
+        }
+      >
+        <li>
+          <Link href="/">
+            <a>Home</a>
           </Link>
-        </>
-      }
-    >
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/#case-studies">
-          <a>Our Work</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/#about">
-          <a>About</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/#footerContactForm">
-          <a>Contact</a>
-        </Link>
-      </li>
-    </CenteredFooter>
-  </Background>
+        </li>
+        <li>
+          <Link href="/#case-studies">
+            <a>Our Work</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/#about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/#footerContactForm">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </CenteredFooter>
+    </Background>
+  </>
 );
 
 export { Footer };
