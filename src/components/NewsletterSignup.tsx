560f810085d057b077c5a8c845cab08a60c7116f
PabloG <p.goldstoff@gmail.com>
2022-10-29 17:35:36 -0300

    Klaviyo

--- src/components/NewsletterSignup.tsx
+++ src/components/NewsletterSignup.tsx
@@ -18,17 +18,23 @@ const NewsletterSignup = () => {
     };
 
     const urlData = new URLSearchParams(data);
+    const feedback = document.querySelector('.news-feedback');
     fetch(`https://manage.kmail-lists.com/ajax/subscriptions/subscribe`, {
       method: 'POST',
       body: urlData,
     }).then((response) => {
-      console.log(urlData);
-      console.log(response);
+      if (response.ok) {
+        if (feedback) {
+          feedback.innerHTML = 'Thank you!';
+        }
+      } else {
+        console.log('error');
+      }
     });
   };
   return (
-    <section className="flex flex-col lg:flex-row justify-center items-center p-4 bg-black">
-      <div className="pb-2">
+    <section className="flex flex-col justify-center items-center  p-6 bg-black">
+      <div className="pb-2 ">
         <Image
           className="object-center invert"
           src="/img/newsletter.svg"
@@ -38,29 +44,29 @@ const NewsletterSignup = () => {
         />
       </div>
 
-      <p className="font-bold text-center !text-white mb-4 lg:mb-0 lg:px-6 ">
+      <p className="font-bold text-center !text-white mb-4 lg:mt-3 lg:px-6 ">
         Subscribe to our Newsletter
       </p>
-      <div className="flex flex-row justify-center ">
-        <form onSubmit={onSubmit}>
-          <input type="email" name="email" placeholder="Email" ref={emailRef} />
-
-          <button type="submit" className="text-red-500">
-            Subscribe to newsletter
-          </button>
-        </form>
-
-        <Script src="//www.klaviyo.com/media/js/public/klaviyo_subscribe.js" />
-
-        <form>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            className="h-full rounded-md mr-2 outline-none placeholder:text-gray-600 pl-4 md:min-w-[300px]"
-          ></input>
-        </form>
-        <Button className="bg-primary-500  text-base">Join</Button>
+      <div>
+        <div className="flex flex-row justify-center ">
+          <form onSubmit={onSubmit}>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Enter your email"
+              ref={emailRef}
+              className="h-full rounded-md mr-2 outline-none placeholder:text-gray-600 pl-4 md:min-w-[300px]"
+            ></input>
+            <Button type="submit" className="bg-primary-500  text-base">
+              Join
+            </Button>
+          </form>
+          <Script src="//www.klaviyo.com/media/js/public/klaviyo_subscribe.js" />
+        </div>
+        <div className="news-feedback text-white font-bold mt-4 text-center ">
+          We will only send you good stuff!
+        </div>
       </div>
     </section>
   );
